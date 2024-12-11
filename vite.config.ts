43fb618834ebf859102dbe18e2d7f986ee41626d
yanluhui <ylh19961209@126.com>
2024-12-11 22:03:11 +0800

    tsconfig配置调整

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig((config) => {
+  console.log("🚀 ~ defineConfig ~ config:", config)
+  return {
+    plugins: [react()],
+  }
 })
