2f771f0d9095be0ff700d20f2bec526ff2d49033
yanluhui <ylh19961209@126.com>
2025-02-21 00:59:44 +0800

    添加zustand-x

--- src/plugins/assets.ts
+++ src/plugins/assets.ts
@@ -1,3 +1,4 @@
 // 提前需要加载的静态资源
 import '../styles/index.css'
+// antd 兼容React19
 import '@ant-design/v5-patch-for-react-19'
--- src/store/index.ts
+++ src/store/index.ts
@@ -12,11 +12,14 @@ interface Action {
   decrement: (qty: number) => void
   changeNest: (qty: number) => void
 }
-export const useMyStore = create<State & Action>(set => ({
+export const useMyStore = create<State & Action>()(set => ({
   count: 0,
   count2: 2,
   nest: { num: 10 },
   increment: (qty: number) => set(state => ({ count: state.count + qty })),
   decrement: (qty: number) => set(state => ({ count: state.count - qty })),
-  changeNest: (qty: number) => set(state => ({ ...state, count: state.nest.num - qty })),
+  changeNest: (qty: number) => set(state => {
+    const num = state.nest.num - qty
+    return { nest: { num } }
+  }),
 }))
